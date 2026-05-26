1fef14832fa8819bacb80f53d2c6b4d3ed3f66fd
keerthisrinivasarao <137041272+keerthisrinivasarao@users.noreply.github.com>
2026-05-26 12:47:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,8 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-
-require("dotenv").config();
-
-const { createClient } = require("@supabase/supabase-js");
-
-const app = express();
-
-app.use(cors());
-
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "public")));
-
-const supabase = createClient(
-    process.env.SUPABASE_URL,
-    process.env.SUPABASE_KEY
-);
-
-app.get("/", (req, res) => {
-
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-
-});
-
 app.post("/login", async (req, res) => {
 
     try{
 
         const { username, mobile } = req.body;
-
-        if(!username || !mobile){
-
-            return res.json({
-                success:false,
-                message:"All fields required"
-            });
-
-        }
 
         const { data:user, error } = await supabase
             .from("users")
@@ -60,7 +24,7 @@ app.post("/login", async (req, res) => {
 
             return res.json({
                 success:false,
-                message:"Invalid Login Details"
+                message:"Invalid Login"
             });
 
         }
@@ -77,13 +41,5 @@ app.post("/login", async (req, res) => {
         });
 
     }
-
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-
-    console.log(`Server Running On ${PORT}`);
 
 });
